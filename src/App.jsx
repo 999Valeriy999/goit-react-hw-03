37745417b4b125272f317620a90dfa2d9cc53fc6
Valeriy Borodenko <143195865+999Valeriy999@users.noreply.github.com>
2024-05-23 23:00:35 +0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,53 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import initialContacts from './components/contacts.json';
+import ContactForm from './components/ContactForm/ContactForm.jsx';
+import ContactList from './components/ContactList/ContactList.jsx';
+import SearchBox from './components/SearchBox/SearchBox.jsx';
+
 
 function App() {
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = localStorage.getItem('contact');
+    if (savedContacts !== null) {
+      return JSON.parse(savedContacts);
+    }
 
+    return initialContacts;
+  });
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const addContact = (newContact) => {
+    setContacts((prevConacts) => {
+      return [...prevConacts, newContact];
+    });
+  };
+
+  const deleteContact = (contactId) => {
+    setContacts((prevConacts) => {
+      return prevConacts.filter((contact) => contact.id !== contactId);
+    });
+  };
+
+  useEffect(() => {
+    localStorage.setItem('contact', JSON.stringify(contacts));
+  }, [contacts]);
+
+
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+  };
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm onAdd={addContact} />
+      <SearchBox handleSearch={handleSearch} />
+
+      <ContactList contacts={filteredContacts} onDelete={deleteContact} />
+    </div>
+  );
 }
+
 
 export default App
