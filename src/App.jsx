3f3d8ee1e670e9335fee47c3f98c4dbdd2dc8e65
Valeriy Borodenko <143195865+999Valeriy999@users.noreply.github.com>
2024-05-25 15:10:29 +0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import initialContacts from './components/contacts.json';
 import ContactForm from './components/ContactForm/ContactForm.jsx';
